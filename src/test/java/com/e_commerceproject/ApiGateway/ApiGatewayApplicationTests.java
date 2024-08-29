fed4076ae58efa98031ed836b0b5633a5a385640
ogbuehi <ogbuehigideon775@gmail.com>
2024-08-29 13:37:04 +0100

    Api gateway

---
+++ src/test/java/com/e_commerceproject/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_commerceproject.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
