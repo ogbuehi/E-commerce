3d4c61ba33a07f6935d46fb8e789a63da62c2341
ogbuehi <ogbuehigideon775@gmail.com>
2024-08-29 12:51:40 +0100

    Eureka Server

---
+++ src/test/java/com/e_commerceproject/EurekerServer/EurekerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_commerceproject.EurekerServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
